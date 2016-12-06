dee1b0cca31dd911c0788defff2fdfc8e3849ee1
malonesk <jeremy.tran@edu.univ-fcomte.fr>
2016-12-06 21:31:22 +0100

    ajout d'une party IA vs IA

--- Diamond/src/Party.java
+++ Diamond/src/Party.java
@@ -18,7 +18,8 @@ public class Party {
     Tree tree;
     BufferedReader consoleIn;
     Scanner input;
-    ArtificialIntelligence IA;
+    ArtificialIntelligence IARed;
+    ArtificialIntelligence IABlue;
 
     public Party() {
         consoleIn = new BufferedReader(new InputStreamReader(System.in));
@@ -45,17 +46,23 @@ public class Party {
             - afficher vainqueur
 
          */
+        /* PLAYER VERSUS PLAYER */
 
-        System.out.println("Bleu : Entrer votre 1er coup (entre 0 et 12) : ");
+        /*initialisation et contruction de l'arbre */
+        /*System.out.println("Bleu : Entrer votre 1er coup (entre 0 et 12) : ");
+
         int coupB1 = input.nextInt();
         tree.setFirstBlueChoice(coupB1);
 
         System.out.println("Rouge : Entrer votre 1er coup (entre 0 et 12) : ");
+
         int coupR1 = input.nextInt();
         tree.setFirstRedChoice(coupR1);
 
         tree.buildTree();
-        IA=new ArtificialIntelligence(tree,board);
+
+        /* ALgorithme de la partie */
+        /*IARed=new ArtificialIntelligence(tree,board);
 
         int turn=2;
         while (turn<12) {
@@ -66,15 +73,53 @@ public class Party {
 
             turn++;
             // IA
-            IA.setB(board);
+            IARed.setB(board);
             //IA.setT(tree);
-            Node nLastPlay=IA.searchLastBluePlayNode(tree.root,(byte)(turn-1));
-            int coupR=IA.computeBestPlay(nLastPlay);
+            Node nLastPlay=IARed.searchLastBluePlayNode(tree.root,(byte)(turn-1));
+            int coupR=IARed.computeBestPlay(nLastPlay);
             board.setPawn(coupR, (byte)(turn/2), (byte)turn);
 
-            System.out.println("rouge : tour "+(turn/2)+ "joue "+coupR);
+            System.out.println("rouge : tour "+(turn/2)+ "\njoue "+coupR);
         }
         board.computeScore();
         System.out.println("bleu : "+board.blueScore+ " rouge : "+board.redScore);
+        */
+        /* IA VS IA
+         */
+        System.out.println("Bleu : Entrer votre 1er coup (entre 0 et 12) : ");
+        int coupB1 = input.nextInt();
+        tree.setFirstBlueChoice(coupB1);
+
+        System.out.println("Rouge : Entrer votre 1er coup (entre 0 et 12) : ");
+
+        int coupR1 = input.nextInt();
+        tree.setFirstRedChoice(coupR1);
+
+        tree.buildTree();
+        IARed=new ArtificialIntelligence(tree,board);
+        IABlue=new ArtificialIntelligence(tree,board);
+        int turn=2;
+        while (turn<12) {
+            // IA Bleue
+            turn++;
+            IABlue.setB(board);
+            Node nLastPlayB=IABlue.searchLastBluePlayNode(tree.root,(byte)(turn-1));
+            int coupB=IABlue.computeBestPlay(nLastPlayB);
+            board.setPawn(coupB,(byte)((turn/2)+1), (byte)turn);
+
+            System.out.println("bleu : tour "+(turn/2)+ "\njoue "+coupB);
+
+            turn++;
+            // IA Rouge
+            IARed.setB(board);
+            Node nLastPlayR=IARed.searchLastBluePlayNode(tree.root,(byte)(turn-1));
+            int coupR=IARed.computeBestPlay(nLastPlayR);
+            board.setPawn(coupR, (byte)(turn/2), (byte)turn);
+
+            System.out.println("rouge : tour "+(turn/2)+ "\njoue "+coupR);
+        }
+        board.computeScore();
+        System.out.println("bleu : "+board.blueScore+ " rouge : "+board.redScore);
+
     }
 }
